b0109236271c9516a77a8c71b4e6bf81ef9adfca
Gabriel <ga.otarola@duocuc.cl>
2024-09-08 22:08:44 -0300

    se actualiza iniciar y cerrar sesion en el menu home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 })
 export class HomePage implements OnInit {
   welcomeMessage: string = '';
+  isLoggedIn: boolean = false;
 
   constructor(
     private router: Router
@@ -19,11 +20,17 @@ export class HomePage implements OnInit {
 
   setWelcomeMessage() {
     const username = localStorage.getItem('username');
+    this.isLoggedIn = !!username;
     this.welcomeMessage = username ? `Bienvenido, ${username}` : 'Bienvenido';
   }
 
   logout() {
     localStorage.removeItem('username');
+    this.isLoggedIn = false; // Cambiamos el estado de login
+    this.welcomeMessage = 'Bienvenido'; // Reseteamos el mensaje de bienvenida
+  }
+
+  goToLogin() {
     this.router.navigate(['/login']);
   }
 }
