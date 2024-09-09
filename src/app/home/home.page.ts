a4719ec172f0ba2694c6f89073fd3c25938f6b8d
Sebastian Diaz <sn.diaz@duocuc.cl>
2024-09-09 01:02:20 -0300

    home ubicacion de menu y flecha

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomePage implements OnInit {
   welcomeMessage: string | null = null;
+  menuOpen: boolean = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private menuCtrl: MenuController) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.welcomeMessage = params['welcome'] ? `Bienvenido, ${params['welcome']}!` : null;
     });
+  }
+
+  closeMenu() {
+    this.menuCtrl.close();  // Cierra el menú
   }
 }
